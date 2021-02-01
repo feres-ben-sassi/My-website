e9240fca973707b9ffaab6880d7c8e3047701c33
Féres Ben Sassi <feresbensassi5@gmail.com>
2021-02-01 14:02:42 +0100

    update 2

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,23 +1,37 @@
-import React, { Component } from 'react'
-import classes from './About.module.css';
-import ScrollAnimation from 'react-animate-on-scroll';
-import "animate.css/animate.min.css"; 
+import React, { Component } from "react";
+import classes from "./About.module.css";
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/animate.min.css";
 
 class About extends Component {
-    render() {
-        return (
-            <div className={classes.box} id="about">
-                <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
-                    <span  className={classes.head}>ABOUT ME</span>
-                    <h2 className={classes.heading}>Who Am I?</h2>
-                    <div className={classes.About}>
-                        <p> I am a 3rd year student currently pursuing my degree in Bachelor of technology in Computer Science and Engineering from Sant Longowal Institute of Engineering and Technology. I want to be a developer as I am interested in developing new things which excite me a lot. :)  </p>
-                         <p className={classes.br}>I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years. </p>
-                    </div>
-                </ScrollAnimation>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={classes.box} id="about">
+        <ScrollAnimation
+          offset="0"
+          animateIn="fadeInLeft"
+          duration="2.4"
+          animateOnce="true"
+          initiallyVisible={true}
+        >
+          <span className={classes.head}>À PROPOS</span>
+          <h2 className={classes.heading}>QUI EST FERES BEN SASSI ?</h2>
+          <div className={classes.About}>
+            <p>
+              {" "}
+              Je m'appel Féres Ben Sassi , un développeur fullstack web et
+              mobile de Nabeul en Tunisie. Actuellement un développeur MERN
+              Stack chez RMG agency.
+            </p>
+            <p className={classes.br}>
+              Je suis doué par les frameworks de JavaScript ainsi que Flutter &
+              React Native pour le développement mobile Cross-plateformes.
+            </p>
+          </div>
+        </ScrollAnimation>
+      </div>
+    );
+  }
 }
 
 export default About;
